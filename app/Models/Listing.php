94a418e4f1650034439a45fdae0f31f97943fd73
ACHii <>
2024-01-09 16:30:32 +0530

    code refactoring

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -2,7 +2,6 @@
 
 namespace App\Models;
 
-use App\Models\Category;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -16,5 +15,10 @@ class Listing extends Model
     public function category(): BelongsTo
     {
         return $this->belongsTo(Category::class);
+    }
+
+    public function listing_status(): BelongsTo
+    {
+        return $this->belongsTo(ListingStatus::class);
     }
 }
